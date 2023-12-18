8d0f6c4a45ed8197db168cab6fcd5965c97d0349
Hugo Cullinan <hugo.cullinan@gmail.com>
2023-12-19 00:25:34 +1100

    render all pokemon with basic stats, add basic css

--- src/components/PokeList.js
+++ src/components/PokeList.js
@@ -1,17 +1,23 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
+import "./PokeList.css";
 
 const PokeList = () => {
-  const [pokeData, setPokeData] = useState(null);
+  const [pokeData, setPokeData] = useState([]);
   const [error, setError] = useState(null);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get(
-          "https://pokeapi.co/api/v2/pokemon/35"
-        );
-        setPokeData(response.data);
+        const promises = [];
+        for (let pokeId = 1; pokeId <= 152; pokeId++) {
+          promises.push(
+            axios.get(`https://pokeapi.co/api/v2/pokemon/${pokeId}`)
+          );
+        }
+        const response = await Promise.all(promises);
+        const data = response.map((response) => response.data);
+        setPokeData(data);
       } catch (err) {
         setError(err);
       }
@@ -20,20 +26,34 @@ const PokeList = () => {
     fetchData();
   }, []);
 
+  console.log(pokeData[1]);
+
   if (error) {
     return <div>Error: {error.message}</div>;
   }
 
   return (
     <div>
-      {pokeData ? (
-        <div>
-          <h1>{pokeData.name}</h1>
-          <img src={pokeData.sprites.front_default} alt={pokeData.name} />
-        </div>
-      ) : (
-        <div>Loading...</div>
-      )}
+      <h1>PokeList</h1>
+      <div className="container">
+        {pokeData.map((pokemon) => (
+          <div className="pokeCard">
+            <h3>{pokemon.name}</h3>
+            <h3>Hp: {pokemon.stats[0].base_stat}</h3>
+            {pokemon.types[1] ? (
+              <h3>
+                Type: {pokemon.types[0].type.name}/{pokemon.types[1].type.name}
+              </h3>
+            ) : (
+              <h3>Type: {pokemon.types[0].type.name}</h3>
+            )}
+
+            <img src={pokemon.sprites.front_default} alt={pokemon.name} />
+            <h3>Atk: {pokemon.stats[1].base_stat}</h3>
+            <h3>Def: {pokemon.stats[2].base_stat}</h3>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
